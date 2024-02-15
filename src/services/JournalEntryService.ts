f14caf1fb1117717199c265c6cb10c595c947ad5
Jon <jonseddon@clear-launch.com>
2024-02-14 20:25:44 -0600

    add journal service

---
+++ src/services/JournalEntryService.ts
@@ -0,0 +1,30 @@
+import JournalEntryModel from "../models/JournalEntry/journalEntryModel";
+import http from './httpService';
+
+class JournalEntryService{
+    public async create(input: JournalEntryModel){
+        let result = await http.post('JournalEntries/create', input)
+        return result.data
+    }
+    public async update(input: JournalEntryModel){
+        let result = await http.put('JournalEntries/{id}',input)
+        return result
+    }
+    public async getTodays(){
+        let result = await http.get('JournalEntries/today')
+        return result;
+    }
+    public async get(){
+
+    }
+
+    public async getAll() {
+
+    }
+
+    public async delete(){
+
+    }
+}
+
+export default new JournalEntryService();
