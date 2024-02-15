dc2296b191130624f8cc5b7ea4c161683eaf4401
Jon <jonseddon@clear-launch.com>
2024-02-14 22:02:04 -0600

    clean up

--- src/Scenes/Home/index.tsx
+++ src/Scenes/Home/index.tsx
@@ -6,7 +6,7 @@ import { SmileOutlined } from '@ant-design/icons';
 import './index.css'
 
 function HomePage() {
-    const {isLoggedIn, setIsLoggedIn, login} = useAuth();
+    const {isLoggedIn} = useAuth();
     const [quote, setQuote] = useState('');
     const [author, setAuthor] = useState('');
 
@@ -26,46 +26,40 @@ function HomePage() {
         fetchQuote();
     }, []);
 
-    const logOut = (e:Event) => {
-        e.preventDefault()
-        setIsLoggedIn(false);
-    }
     return ( 
-        <>
-
-    <Row gutter={16}>
-      <Col span={12}>
+    <>
+        <Row gutter={16}>
+        <Col span={12}>
+            <Card className='card'>
+            <Result
+                icon={<SmileOutlined />}
+                title="Great job at journaling, keep it up!"
+                extra={<Button type="primary">Update</Button>}
+            />
+            </Card> 
+        </Col>
+        <Col span={12}>
         <Card className='card'>
-        <Result
-            icon={<SmileOutlined />}
-            title="Great job at journaling, keep it up!"
-            extra={<Button type="primary">Update</Button>}
-        />
-        </Card> 
-      </Col>
-      <Col span={12}>
-      <Card className='card'>
-        <div style={{display:"flex"}}><h3>{quote}</h3></div>
-        <div style={{display:"flex", justifyContent:"end"}}><h3>-{author}</h3></div>
-        </Card> 
-      </Col>
-    </Row>
-    <Row gutter={16}>
-      <Col span={12}>
+            <div style={{display:"flex"}}><h3>{quote}</h3></div>
+            <div style={{display:"flex", justifyContent:"end"}}><h3>-{author}</h3></div>
+            </Card> 
+        </Col>
+        </Row>
+        <Row gutter={16}>
+        <Col span={12}>
+            <Card className='card'>
+            <div style={{display:"flex"}}><h3>{quote}</h3></div>
+            <div style={{display:"flex", justifyContent:"end"}}><h3>-{author}</h3></div>
+            </Card> 
+        </Col>
+        <Col span={12}>
         <Card className='card'>
-        <div style={{display:"flex"}}><h3>{quote}</h3></div>
-        <div style={{display:"flex", justifyContent:"end"}}><h3>-{author}</h3></div>
-        </Card> 
-      </Col>
-      <Col span={12}>
-      <Card className='card'>
-        <div style={{display:"flex"}}><h3>{quote}</h3></div>
-        <div style={{display:"flex", justifyContent:"end"}}><h3>-{author}</h3></div>
-        </Card> 
-      </Col>
-    </Row>
-
-        </>
+            <div style={{display:"flex"}}><h3>{quote}</h3></div>
+            <div style={{display:"flex", justifyContent:"end"}}><h3>-{author}</h3></div>
+            </Card> 
+        </Col>
+        </Row>
+    </>
 
     );
 }
